bdb8314638c022600749b83bbc0e827381a311b9
Facundo Alvarez <turcoalv@gmail.com>
2020-07-09 14:20:18 -0300

    [ADD] dashboard routes added

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import SignUp from '../views/SignUp.vue';
 import Login from '../views/Login.vue';
+import Dashboard from '../views/Dashboard.vue'
 
 
 import 'bootswatch/dist/darkly/bootstrap.css';
@@ -24,6 +25,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
   }
 ];
 
