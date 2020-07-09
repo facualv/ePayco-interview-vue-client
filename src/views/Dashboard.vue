92f7c22c9ee5f97a2ee47fb09f80ea45d9afaed2
Facundo Alvarez <turcoalv@gmail.com>
2020-07-09 14:18:28 -0300

    [ADD] dashboard template added

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <h1 class="display-3">PANEL DE CONTROL</h1>
+      <p class="lead">A traves de este panel podras hacer pagos y recargar tu billetera</p>
+      <hr class="my-4" />
+      <p>Si y solo si tienes dinero en la cuenta obviamente</p>
+      <p class="lead">
+        <!-- <router-link class="btn btn-primary btn-lg" :to="{ name: 'signup'}" role="button">Pgar</router-link> -->
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Dashboar",
+  components: {}
+};
+</script>
